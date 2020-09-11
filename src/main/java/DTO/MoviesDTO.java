b9dbeebb38b0a465f1be5864e0de1e03e87c247f
GGGE99 <marc.g.ekstrom@gmail.com>
2020-09-11 11:01:41 +0200

    opgaver

--- src/main/java/DTO/MoviesDTO.java
+++ src/main/java/DTO/MoviesDTO.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DTO;
 
-/**
- *
- * @author marcg
- */
+import entities.Movie;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoviesDTO {
-    
+
+    private List<MovieDTO> all = new ArrayList();
+
+    public MoviesDTO(List<Movie> movieList) {
+        for (Movie m : movieList) {
+            all.add(new MovieDTO(m));
+        }
+    }
+
+    public List<MovieDTO> getAll() {
+        return all;
+    }
 }
